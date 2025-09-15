c8ab3691c9c277c331d076f900fe3381fa593967
brunaportelaa <brunaporteladj@gmail.com>
2025-09-14 22:51:27 -0300

    Projeto Restaurante - Construção de Entidades

---
+++ RestauranteJPA/src/main/java/model/Dish.java
@@ -0,0 +1,93 @@
+package ProjetoRestaurante.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Dish {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (nullable = false, length = 200)
+    private String name;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column
+    private boolean vegetarian;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chef_id")
+    private Chef chef;
+
+    @Column(length = 50)
+    private String category;
+
+    public Dish() {
+    }
+
+    public Dish(Long id, String name, double price, boolean vegetarian, Chef chef, String category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.vegetarian = vegetarian;
+        this.chef = chef;
+        this.category = category;
+    }
+
+    public Dish(String name, double price, boolean vegetarian, Chef chef, String category) {
+        this.name = name;
+        this.price = price;
+        this.vegetarian = vegetarian;
+        this.chef = chef;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isVegetarian() {
+        return vegetarian;
+    }
+
+    public void setVegetarian(boolean vegetarian) {
+        this.vegetarian = vegetarian;
+    }
+
+    public Chef getChef() {
+        return chef;
+    }
+
+    public void setChef(Chef chef) {
+        this.chef = chef;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
